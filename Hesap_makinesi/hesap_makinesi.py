af3ebdcfc41c9fa25f82c577b2c9f15e0e3968db
TheKoray18 <korayldrm18@gmail.com>
2021-03-21 12:50:21 +0300

    Update hesap_makinesi.py

--- Hesap_makinesi/hesap_makinesi.py
+++ Hesap_makinesi/hesap_makinesi.py
@@ -19,16 +19,17 @@ class Window(QtWidgets.QWidget):
     def pencere(self):
         
         self.setGeometry(200, 200, 500, 500)
-        self.setWindowTitle("ToplaÇıkar")
-        
-        self.buton_topla = QtWidgets.QPushButton("Topla")
-        self.buton_çıkar = QtWidgets.QPushButton("Çıkar")
+        self.setWindowTitle("HesapMakinesi")
+      
+        self.buton_topla = QtWidgets.QPushButton("Toplama")
+        self.buton_çıkar = QtWidgets.QPushButton("Çıkarma")
+        self.buton_çarp = QtWidgets.QPushButton("Çarpma")
+        self.buton_böl = QtWidgets.QPushButton("Bölme")
         self.yenile = QtWidgets.QPushButton("Yenile")
         self.yazı_alanı1 = QtWidgets.QLineEdit()
         
         self.yazı_alanı2 = QtWidgets.QLineEdit()
-        
-        
+              
         self.sonuc = QtWidgets.QLabel()
         self.sonuc.setText("Sonuç")
         
@@ -42,6 +43,8 @@ class Window(QtWidgets.QWidget):
         v_box.addWidget(self.yenile)
         v_box.addWidget(self.buton_topla)
         v_box.addWidget(self.buton_çıkar)
+        v_box.addWidget(self.buton_çarp)
+        v_box.addWidget(self.buton_böl)
         
         self.setLayout(v_box)
         
@@ -50,23 +53,36 @@ class Window(QtWidgets.QWidget):
         self.yenile.clicked.connect(self.yeni)
         self.buton_topla.clicked.connect(self.topla)
         self.buton_çıkar.clicked.connect(self.cikar)
+        self.buton_çarp.clicked.connect(self.carp)
+        self.buton_böl.clicked.connect(self.bol)
         
     def yeni(self):
         
         self.yazı_alanı1.clear()
         self.yazı_alanı2.clear()
+        self.sonuc.clear()
     
     def topla(self):
         
         self.result = int(self.yazı_alanı1.text()) + int(self.yazı_alanı2.text())
         self.sonuc.setText(str(self.result))
-       
-        
+              
     def cikar(self):
         
         self.result = int(self.yazı_alanı1.text()) - int(self.yazı_alanı2.text())
         self.sonuc.setText(str(self.result))
         
+    def carp(self):
+        
+        self.result = int(self.yazı_alanı1.text()) * int(self.yazı_alanı2.text())
+        self.sonuc.setText(str(self.result))
+        
+    def bol(self):
+        
+        self.result = int(self.yazı_alanı1.text()) / int(self.yazı_alanı2.text())       
+        self.sonuc.setText(str(self.result))
+        
+     
 app = QtWidgets.QApplication(sys.argv)
 
 window = Window()
